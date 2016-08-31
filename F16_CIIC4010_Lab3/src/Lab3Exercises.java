7d87872e93c7f5e51cce0bc214613d6fec07493f
4015d2 <4015d2@zapote.ece.uprm.edu>
2016-08-31 14:34:51 -0400

    a BLUE SQUEARE WITH  A GREEN AND BLACK BORDER

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,10 +2,13 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("BLAS ANDRES AYALA GARCIA!");
+		JFrame myFrame = new JFrame("BLAS ANDRES AYALA GARCIA");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(900, 400);
 		myFrame.setSize(200, 200);
+		
+		 MyPanelClass myPanel = new MyPanelClass();
+                        myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 	}
 }
